86efe2cd8f6aa3e76236c3401af7eb04d6dcff66
Selvam42 <140913494+Selvam42@users.noreply.github.com>
2024-03-01 22:52:17 +0530

    Add files via upload

---
+++ tic tac toe/toe.js
@@ -0,0 +1,89 @@
+const boxs=document.querySelectorAll('.box');
+const statust=document.querySelector('#status');
+const btnRestart=document.querySelector('#restart');
+let x="<img src='x.jpg'>";
+let o="<img src='o.jpg'>";
+
+const winPossiblity=[
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+let options=["","","","","","","","",""];
+let currentPlayer=x;
+let player="x";
+let running=false;
+init();
+function init(){
+    boxs.forEach(box=>box.addEventListener('click',boxClick));
+    btnRestart.addEventListener('click',restartGame);
+    // Change from single quotes to backticks
+    statust.textContent=`${player} your turn`;
+    running=true;
+}
+function boxClick(){
+    const index = this.dataset.index;
+    if(options[index]!="" || !running)
+    {
+        return;
+    }
+    updateBox(this,index);
+    checkWinner();
+}
+function updateBox(box,index){
+    options[index]=player;
+    box.innerHTML=currentPlayer;
+
+}
+function changePlayer(){
+    player=(player=='x') ? "o" :"x";
+    currentPlayer=(currentPlayer==x) ? o :x;
+    statust.textContent = `${player} your turn`;
+}
+function checkWinner(){
+    let isWon=false;
+    for(let i=0;i<winPossiblity.length;i++){
+        const condition=winPossiblity[i];
+        const box1=options[condition[0]];
+        const box2=options[condition[1]];
+        const box3=options[condition[2]];
+        if(box1=="" || box2=="" || box3==""){
+            continue;
+        }
+        if(box1==box2 && box2==box3){
+            isWon=true;
+            boxs[condition[0]].classList.add('winPossiblity');
+            boxs[condition[1]].classList.add('winPossiblity');
+            boxs[condition[2]].classList.add('winPossiblity');
+        }
+    }
+    if(isWon){
+        statust.textContent = `${player} won . . .`;
+        running=false;
+    }
+    else if(!options.includes("")){
+        statust.textContent='game draw. . !';
+        running=false;
+    }
+    else{
+        changePlayer();
+    }
+
+}
+function restartGame(){ 
+options=["","","","","","","","",""];
+currentPlayer=x;
+player="x";
+running=true;
+statust.textContent = `${player} your turn`;
+//statust.textContent = `${player} your turn`;
+boxs.forEach(box=>{
+    box.innerHTML="";
+    box.classList.remove('winPossiblity');
+});
+}
